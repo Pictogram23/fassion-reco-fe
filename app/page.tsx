afc17279cc5258bfb7fb50f5a490c058a83b710c
Pictogram23 <shanchengyinger@gmail.com>
2025-05-13 05:52:43 +0900

    APIとの繋ぎこみを実装

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,46 @@
 
 import { Coordinate } from "@/components/Coordinate";
 import { Header } from "@/components/Header";
+import { API_BASE_URL } from "@/const";
 import { Button, Container, Flex, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
+type Response = {
+	result: number;
+};
+
 export default function Home() {
 	const [review, setReview] = useState<number | undefined>();
+	const [tops, setTops] = useState<number[]>();
+	const [bottoms, setBottoms] = useState<number[]>();
+
+	const handleSubmit = async () => {
+		if (!tops || !bottoms) return;
+		const res = await fetch(`${API_BASE_URL}/`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ tops: tops, bottoms: bottoms }),
+		});
+		const data: Response = await res.json();
+		setReview(data.result);
+	};
 
 	return (
 		<>
 			<Header />
 			<Container mt={5}>
 				<Flex>
-					<Coordinate label="トップス" />
+					<Coordinate label="トップス" onChange={(e) => setTops(e)} />
 				</Flex>
 				<Flex mt={5}>
-					<Coordinate label="ボトムス" />
+					<Coordinate label="ボトムス" onChange={(e) => setBottoms(e)} />
 				</Flex>
 				<Flex mt={5}>
-					<Button bg={"blue.500"}>送信</Button>
+					<Button bg={"blue.500"} onClick={handleSubmit}>
+						送信
+					</Button>
 				</Flex>
 				<Flex mt={10}>
 					<Text>あなたのコーディネートは {review} 点です</Text>
--- components/Coordinate.tsx
+++ components/Coordinate.tsx
@@ -2,11 +2,20 @@ import { ColorPicker, HStack, Heading, Portal } from "@chakra-ui/react";
 
 type Props = {
 	label: string;
+	onChange: (value: number[]) => void;
 };
 
-export function Coordinate({ label }: Props) {
+export function Coordinate({ label, onChange }: Props) {
+	const handleChange = (event: React.FormEvent<HTMLDivElement>) => {
+		const value: string = event.target.value;
+		const slicedValue = value.slice(5).slice(0, -1).replaceAll(" ", "");
+		const splitedValue = slicedValue.split(",");
+		const encodedValue = splitedValue.map(Number);
+		onChange(encodedValue);
+	};
+
 	return (
-		<ColorPicker.Root>
+		<ColorPicker.Root onChange={(e) => handleChange(e)}>
 			<ColorPicker.HiddenInput />
 			<ColorPicker.Label>
 				<Heading>{label}</Heading>
